f8b9868921c5709e70fb7806c7cea9faa8caa97d
Jake Meyers <jakejake92@yahoo.com>
2020-09-18 16:31:38 -0600

    app complete

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/YouTube";
 import VideoList from "./VideosList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
     state = { videos: [ ], selectedVideo: null };
+
+    componentDidMount() {
+        this.onTermSubmit('Web Development');
+    }
 
    onTermSubmit = async term => {
         const response = await youtube.get('/search', {
@@ -13,11 +18,14 @@ class App extends React.Component {
             }
         });
 
-        this.setState( { videos: response.data.items } );
+        this.setState( { 
+            videos: response.data.items, 
+            selectedVideo: response.data.items[0]
+        } );
     };
 
     onVideoSelect = (video) => {
-        console.log('from the app', video);
+        this.setState({selectedVideo: video})
     }
    
     render() {
@@ -25,7 +33,14 @@ class App extends React.Component {
         return (
             <div className = "ui container">
                 <SearchBar  onFormSubmit={ this.onTermSubmit } />
-                <VideoList  onVideoSelect = {this.onVideoSelect}  videos = { this.state.videos }/>
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column"><VideoDetail video = {this.state.selectedVideo} /></div>
+                        <div className="5 wide column"><VideoList  onVideoSelect = {this.onVideoSelect}  videos = { this.state.videos }/></div>
+                        
+                        
+                    </div>
+                </div>
             </div>
         );
     }
