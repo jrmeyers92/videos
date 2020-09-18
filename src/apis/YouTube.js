0e693e1da7cadab189742962b3cea33d3a4b173c
Jake Meyers <jakejake92@yahoo.com>
2020-09-17 21:42:42 -0600

    video list created

--- src/apis/YouTube.js
+++ src/apis/YouTube.js
@@ -1,1 +1,16 @@
-const KEY = AIzaSyC9vZ1JAE5KW9ajxe8BzRwGDfiAezMN9_Q;
+import axios from "axios";
+
+
+const KEY = 'AIzaSyC9vZ1JAE5KW9ajxe8BzRwGDfiAezMN9_Q';
+
+
+
+export default axios.create({
+    baseURL: "https://www.googleapis.com/youtube/v3",
+    params: {
+        part: "snippet",
+        type: "video",
+        maxResults: 5,
+        key: KEY
+    }
+});
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,6 +9,9 @@ class SearchBar extends React.Component {
 
     onFormSubmit = event => {
         event.preventDefault();
+
+        // TODO: make sure we call callback from parent component
+        this.props.onFormSubmit(this.state.term);
     };
 
     render() {
